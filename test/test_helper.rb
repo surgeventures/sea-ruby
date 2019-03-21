998981e4e79655657c040c3ff6f8686e27a0cbf8
Karol Słuszniak <ksluszniak@gmail.com>
2019-03-21 12:22:12 +0100

    Configure codecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,11 @@
 require 'simplecov'
 SimpleCov.start
 
+if ENV.key?("CODECOV_TOKEN")
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
 require 'minitest/autorun'
 require 'sea'
 
